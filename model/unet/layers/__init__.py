7ca3e46c0c59a6e70bb007486903b50e3263fa50
Johannes Fischer <joh.fischer@outlook.com>
2022-07-28 11:16:07 +0200

    add efficient attention

--- model/unet/layers/__init__.py
+++ model/unet/layers/__init__.py
@@ -1,4 +1,3 @@
-from .swish import Swish
 from .embedding import TimeEmbedding
 from .residual import ResidualBlock
-from .attention import SelfAttention
+from .attention import SelfAttention, LinearAttention
--- model/unet/layers/attention.py
+++ model/unet/layers/attention.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from einops import rearrange
 
 
 class SelfAttention(nn.Module):
@@ -26,12 +27,57 @@ class SelfAttention(nn.Module):
         return x
 
 
+class LinearAttention(nn.Module):
+    def __init__(self, n_channels, n_heads: int = 4, d_k: int = 32):
+        """
+        Efficient Attention (https://arxiv.org/abs/1812.01243), which instead of
+        computing V (Q K.T) like in dot-product attention, computes Q (K.T V).
+        This results in less complexity, O(d_k * d_v) instead of O(n²).
+
+        Args:
+            n_channels (int): Number of channels of the input feature maps
+            n_heads (int): Number of heads for attention
+            d_k (int): Dimension of queries, keys, and values
+        """
+        super().__init__()
+        self.scale = d_k ** -0.5
+        self.n_heads = n_heads
+        hidden_dim = d_k * n_heads
+        self.to_qkv = nn.Conv2d(n_channels, hidden_dim * 3, 1, bias=False)
+
+        self.to_out = nn.Sequential(nn.Conv2d(hidden_dim, n_channels, 1),
+                                    nn.GroupNorm(1, n_channels))
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        q, k, v = map(
+            lambda t: rearrange(t, "b (h c) x y -> b h c (x y)", h=self.n_heads), qkv
+        )
+
+        q = q.softmax(dim=-2)
+        k = k.softmax(dim=-1)
+
+        q = q * self.scale
+        context = torch.einsum("b h d n, b h e n -> b h d e", k, v)
+
+        res = torch.einsum("b h d e, b h d n -> b h e n", context, q)
+        res = rearrange(res, "b h c (x y) -> b (h c) x y", h=self.n_heads, x=h, y=w)
+
+        return self.to_out(res)
+
+
 if __name__ == "__main__":
     ipt = torch.randn((4, 32, 16, 16))
 
-    attention = SelfAttention(32, 2)
+    self_attn = SelfAttention(d_k=32, n_heads=2)
+    out = self_attn(ipt)
+    print("Self Attention")
+    print("\tInput:", ipt.shape)
+    print("\tOutput:", out.shape)
 
-    out = attention(ipt)
-
-    print("Input:", ipt.shape)
-    print("Output:", out.shape)
+    lin_attn = LinearAttention(32, n_heads=2, d_k=32)
+    out = lin_attn(ipt)
+    print("Linear Attention")
+    print("\tInput:", ipt.shape)
+    print("\tOutput:", out.shape)
