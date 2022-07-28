2295000313a04821e6615ff8cd0fc56dd2ca5d23
Johannes Fischer <joh.fischer@outlook.com>
2022-07-28 09:44:56 +0200

    adapted positional embedding

--- model/unet/layers/embedding.py
+++ model/unet/layers/embedding.py
@@ -1,40 +1,39 @@
 import torch
 import torch.nn as nn
-from model.unet.layers import Swish
 
 
 class TimeEmbedding(nn.Module):
-    def __init__(self, time_channels: int, embedding_dim: int, max_len: int = 5000):
+    def __init__(self, time_emb_dim: int, pos_emb_dim: int, max_len: int = 5000):
         """
         Time embedding for time step t. First, t is embedded using a fixed sinusoidal positional
         embedding, as described in "Attention Is All You Need" (https://arxiv.org/abs/1706.03762),
         followed by a two layer MLP.
 
         Args:
-            time_channels: Dimension of final time embedding
-            embedding_dim: Embedding dimension for the fixed sinusoidal positional embedding
+            time_emb_dim: Dimension of final time embedding
+            pos_emb_dim: Embedding dimension for the fixed sinusoidal positional embedding
             max_len: Maximum number of time steps (default: 5000)
         """
         super().__init__()
 
-        self.embedding_dim = embedding_dim
-        self.n_channels = time_channels
+        self.pos_emb_dim = pos_emb_dim
+        self.time_emb_dim = time_emb_dim
         self.max_len = max_len
 
         # fixed sinusoidal positional embedding
-        assert self.embedding_dim % 2 == 0, "Embedding dim must be a multiple of 2!"
+        assert self.pos_emb_dim % 2 == 0, "Embedding dim must be a multiple of 2!"
         pos = torch.arange(0, max_len).float().unsqueeze(1)
-        _2i = torch.arange(0, self.embedding_dim, 2).float()
-        pos_embedding = torch.zeros(self.max_len, self.embedding_dim)
-        pos_embedding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / self.embedding_dim)))
-        pos_embedding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / self.embedding_dim)))
+        _2i = torch.arange(0, self.pos_emb_dim, 2).float()
+        pos_embedding = torch.zeros(self.max_len, self.pos_emb_dim)
+        pos_embedding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / self.pos_emb_dim)))
+        pos_embedding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / self.pos_emb_dim)))
         self.register_buffer('pos_embedding', pos_embedding, persistent=True)
 
         # MLP for time embedding
         self.mlp = nn.Sequential(
-            nn.Linear(self.embedding_dim, self.n_channels),
-            Swish(beta=1.0),
-            nn.Linear(self.n_channels, self.n_channels)
+            nn.Linear(self.pos_emb_dim, self.time_emb_dim),
+            nn.SiLU(),
+            nn.Linear(self.time_emb_dim, self.time_emb_dim)
         )
 
         print(self.pos_embedding.device)
@@ -57,11 +56,10 @@ class TimeEmbedding(nn.Module):
 
 
 if __name__ == "__main__":
-    emb = TimeEmbedding(16, embedding_dim=4, max_len=10)
-    print("Fixed positional embedding:\n", emb.pos_embedding)
-
+    emb = TimeEmbedding(time_emb_dim=16, pos_emb_dim=4, max_len=10)
     time = torch.randint(0, 10, (4,))
-    print("Time:", time)       # eg [2, 5, 7, 9]
-
     time_emb = emb.forward(time)
+
+    print("Fixed positional embedding:\n", emb.pos_embedding)
+    print("Time:", time)                        # eg [2, 5, 7, 9]
     print("Time embedding:", time_emb.shape)    # [4, 16]
