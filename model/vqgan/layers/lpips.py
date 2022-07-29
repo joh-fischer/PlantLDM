e7fad6b75806d9508196b51c18ab61bffea1395f
Johannes Fischer <joh.fischer@outlook.com>
2022-07-29 17:49:31 +0200

    download vgg.pth

--- model/vqgan/layers/lpips.py
+++ model/vqgan/layers/lpips.py
@@ -3,12 +3,51 @@
 # https://github.com/CompVis/taming-transformers/blob/24268930bf1dce879235a7fddd0b2355b84d7ea6/taming/modules/losses/vqperceptual.py
 # https://github.com/CompVis/taming-transformers/blob/24268930bf1dce879235a7fddd0b2355b84d7ea6/taming/modules/losses/lpips.py
 
+import os, hashlib
+import requests
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 from torchvision import models
 from collections import namedtuple
 
 
+""" From utils """
+URL_MAP = {"vgg_lpips": "https://heibox.uni-heidelberg.de/f/607503859c864bc1b30b/?dl=1"}
+CKPT_MAP = {"vgg_lpips": "vgg.pth"}
+MD5_MAP = {"vgg_lpips": "d507d7349b931f0638a25a48a722f98a"}
+
+
+def download(url, local_path, chunk_size=1024):
+    os.makedirs(os.path.split(local_path)[0], exist_ok=True)
+    with requests.get(url, stream=True) as r:
+        total_size = int(r.headers.get("content-length", 0))
+        with tqdm(total=total_size, unit="B", unit_scale=True) as pbar:
+            with open(local_path, "wb") as f:
+                for data in r.iter_content(chunk_size=chunk_size):
+                    if data:
+                        f.write(data)
+                        pbar.update(chunk_size)
+
+
+def md5_hash(path):
+    with open(path, "rb") as f:
+        content = f.read()
+    return hashlib.md5(content).hexdigest()
+
+
+def get_ckpt_path(name, root, check=False):
+    assert name in URL_MAP
+    path = os.path.join(root, CKPT_MAP[name])
+    if not os.path.exists(path) or (check and not md5_hash(path) == MD5_MAP[name]):
+        print("Downloading {} model from {} to {}".format(name, URL_MAP[name], path))
+        download(URL_MAP[name], path)
+        md5 = md5_hash(path)
+        assert md5 == MD5_MAP[name], md5
+    return path
+
+
+""" From LPIPS """
 class LPIPS(nn.Module):
     # Learned perceptual metric
     def __init__(self, use_dropout=True):
@@ -26,6 +65,7 @@ class LPIPS(nn.Module):
             param.requires_grad = False
 
     def load_from_pretrained(self, name="vgg_lpips"):
+        # download vgg.pth
         ckpt = get_ckpt_path(name, "taming/modules/autoencoder/lpips")
         self.load_state_dict(torch.load(ckpt, map_location=torch.device("cpu")), strict=False)
         print("loaded pretrained LPIPS loss from {}".format(ckpt))
@@ -121,3 +161,11 @@ def normalize_tensor(x,eps=1e-10):
 
 def spatial_average(x, keepdim=True):
     return x.mean([2, 3], keepdim=keepdim)
+
+
+if __name__ == "__main__":
+    name = 'vgg_lpips'
+    # ckpt = get_ckpt_path(name, "taming/modules/autoencoder/lpips")
+    # https://heibox.uni-heidelberg.de/f/607503859c864bc1b30b/?dl=1
+    # just downloads vgg.pth
+    ckpt = get_ckpt_path("vgg_lpips", "")
