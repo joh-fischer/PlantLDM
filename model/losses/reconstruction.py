303a8c65bf57b2c520a056002d6cef60fc5ba21c
Johannes Fischer <joh.fischer@outlook.com>
2022-08-04 10:54:00 +0200

    add reconstruction loss

--- model/losses/reconstruction.py
+++ model/losses/reconstruction.py
@@ -3,21 +3,21 @@ import torch.nn as nn
 
 
 class ReconstructionLoss(nn.Module):
-    def __init__(self, loss_type: str):
+    def __init__(self, rec_loss_type: str = 'L1'):
         """
         Computes the reconstruction loss.
 
         Args:
-            loss_type: Either L1 or L2.
+            rec_loss_type: Either L1 or L2.
         """
         super().__init__()
 
-        if loss_type.lower() == 'l1':
+        if rec_loss_type.lower() == 'l1':
             self.rec_loss_fn = torch.nn.functional.l1_loss
-        elif loss_type.lower() == 'l2':
+        elif rec_loss_type.lower() == 'l2':
             self.rec_loss_fn = torch.nn.functional.mse_loss
         else:
-            raise ValueError(f"Unknown reconstruction loss type '{loss_type}'!")
+            raise ValueError(f"Unknown reconstruction loss type '{rec_loss_type}'!")
 
     def forward(self, x_hat: torch.Tensor, x: torch.Tensor):
         rec_loss = self.rec_loss_fn(x_hat, x)
@@ -29,6 +29,6 @@ if __name__ == "__main__":
     ipt = torch.randn((8, 3, 32, 32))
     tgt = torch.randn((8, 3, 32, 32))
 
-    loss_fn = ReconstructionLoss('L1')
+    loss_fn = ReconstructionLoss('L2')
     print("Diff:", loss_fn(ipt, tgt).item())
     print("Same:", loss_fn(ipt, ipt).item())
