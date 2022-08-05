919f815ec9eb4cc1c4f1fa4f0d2e5edff809d61d
Johannes Fischer <joh.fischer@outlook.com>
2022-08-05 11:43:17 +0200

    test discriminator loss

--- model/losses/loss.py
+++ model/losses/loss.py
@@ -26,6 +26,10 @@ class LossFn(nn.Module):
             perceptual_weight: Weight for the perceptual LPIPS loss.
             codebook_weight: Weight for the codebook loss of the vector quantizer.
             commitment_weight: Beta for the commitment loss of the vector quantizer.
+            disc_weight: Weight for the adversarial loss.
+            disc_in_channels: Input channels for the discriminator.
+            disc_n_layers: Number of layers in the discriminator.
+            disc_res_blocks: If true, adds residual blocks to the discriminator.
         """
         super().__init__()
         # reconstruction loss (take L1 as it is produces less blurry
@@ -47,23 +51,25 @@ class LossFn(nn.Module):
         # (according to https://arxiv.org/abs/1611.07004)
         self.disc_weight = disc_weight
         self.discriminator = Discriminator(disc_in_channels, n_layers=disc_n_layers,
-                                           residual_blocks=disc_res_blocks) if disc_weight > 0 else None
+                                           residual_blocks=disc_res_blocks
+                                           ) if disc_weight > 0 else None
+        self.opt_disc = torch.optim.Adam(self.discriminator.parameters(), lr=0.0002
+                                         ) if disc_weight > 0 else None
 
     def forward(self, x_hat: torch.Tensor, x: torch.Tensor,
-                z_e: torch.Tensor, z_q: torch.Tensor,
-                train_autoencoder: bool = True):
+                z_e: torch.Tensor, z_q: torch.Tensor):
         """
         Computes the final loss including the following sub-losses:
         - reconstruction loss
         - codebook loss for vector quantizer
         - perceptual loss using LPIPS
+        - adversarial loss with discriminator
 
         Args:
             x_hat: Reconstructed image.
             x: Original image.
             z_e: Encoded image.
             z_q: Quantized encoded image.
-            train_autoencoder: If false, computes discriminator loss, else generator loss.
 
         Returns:
             loss: The combined loss.
@@ -74,48 +80,79 @@ class LossFn(nn.Module):
         loss = torch.tensor([0.0]).to(device)
 
         # loss for generator / autoencoder
-        if train_autoencoder:
-            rec_loss = self.rec_loss_fn(x_hat, x)
-            loss += rec_loss
-            log['rec_loss'] = rec_loss.item()
+        rec_loss = self.rec_loss_fn(x_hat, x)
+        loss += rec_loss
+        log['rec_loss'] = rec_loss.item()
 
-            codebook_loss = self.codebook_weight * self.codebook_loss_fn(z_e, z_q)
-            loss += codebook_loss
-            log['codebook_loss'] = codebook_loss.item()
+        codebook_loss = self.codebook_weight * self.codebook_loss_fn(z_e, z_q)
+        loss += codebook_loss
+        log['codebook_loss'] = codebook_loss.item()
 
-            if self.perceptual_loss_fn is not None:
-                perceptual_loss = self.perceptual_loss_fn(x_hat, x)
-                perceptual_loss *= self.perceptual_weight
-                loss += perceptual_loss
-                log['perceptual_loss'] = perceptual_loss.item()
+        if self.perceptual_loss_fn is not None:
+            perceptual_loss = self.perceptual_loss_fn(x_hat, x)
+            perceptual_loss *= self.perceptual_weight
+            loss += perceptual_loss
+            log['perceptual_loss'] = perceptual_loss.item()
 
-            if self.discriminator is not None:
-                disc_out_fake = self.discriminator(x_hat)
-                target_fake = torch.ones(disc_out_fake.shape).to(device)
-                generator_loss = nn.functional.binary_cross_entropy(disc_out_fake, target_fake)
-                generator_loss *= self.disc_weight
-                loss += generator_loss
-                log['generator_loss'] = generator_loss.item()
+        if self.disc_weight > 0:
+            disc_out_fake = self.discriminator(x_hat)
+            target_fake = torch.ones(disc_out_fake.shape).to(device)
+            generator_loss = nn.functional.binary_cross_entropy(disc_out_fake, target_fake)
+            generator_loss *= self.disc_weight
+            loss += generator_loss
+            log['generator_loss'] = generator_loss.item()
 
-            log['loss'] = loss.item()
-        # loss for discriminator
-        else:
-            # fake output
-            disc_out_fake = self.discriminator(x_hat.detach())
-            target_fake = torch.zeros(disc_out_fake.shape).to(device)
-            disc_loss_fake = nn.functional.binary_cross_entropy(disc_out_fake, target_fake)
-            log['disc_loss_fake'] = disc_loss_fake.item()
+        log['loss'] = loss.item()
 
-            # real output
-            disc_out_real = self.discriminator(x.detach())
-            target_real = torch.ones(disc_out_real.shape).to(device)
-            disc_loss_real = nn.functional.binary_cross_entropy(disc_out_real, target_real)
-            log['disc_loss_real'] = disc_loss_real.item()
+        return loss, log
 
-            disc_loss = 0.5 * (disc_loss_real + disc_loss_fake)
-            disc_loss *= self.disc_weight
-            log['disc_loss'] = disc_loss.item()
-            loss += disc_loss
+    def update_discriminator(self, x_hat: torch.Tensor, x: torch.Tensor):
+        """
+        Updates the discriminator based on the original images and reconstructions.
+
+        Args:
+            x_hat: Reconstructed images.
+            x: Original images.
+
+        Returns:
+            loss: The loss of the discriminator for fake (x_hat) and real (x) images.
+            log: A dictionary containing all sub-losses and the total loss.
+        """
+        device = x.device
+        log = {}
+
+        self.discriminator.zero_grad()
+
+        """ Train all real batch """
+        # Forward pass real batch through D
+        disc_out_real = self.discriminator(x)
+        target_real = torch.ones(disc_out_real.shape).to(device)
+
+        # Calculate loss on all-real batch
+        disc_loss_real = nn.functional.binary_cross_entropy(disc_out_real, target_real)
+
+        # Calculate gradients for D in backward pass
+        disc_loss_real.backward()
+        log['disc_loss_real'] = disc_loss_real.item()
+
+        """ Train all fake batch """
+        # Classify all fake batch with D
+        disc_out_fake = self.discriminator(x_hat.detach())
+        target_fake = torch.zeros(disc_out_fake.shape).to(device)
+
+        # Calculate D's loss on the all-fake batch
+        disc_loss_fake = nn.functional.binary_cross_entropy(disc_out_fake, target_fake)
+
+        # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+        disc_loss_fake.backward()
+        log['disc_loss_fake'] = disc_loss_fake.item()
+
+        loss = disc_loss_fake + disc_loss_real
+        log['disc_loss'] = loss.item()
+
+        """ Update the discriminator """
+        # Update D
+        self.opt_disc.step()
 
         return loss, log
 
@@ -124,14 +161,15 @@ if __name__ == "__main__":
     l_vqgan = LossFn(perceptual_weight=1)
 
     ipt = torch.randn((8, 3, 128, 128))
-
     latent_e = torch.randn((8, 10, 32, 32))
     latent_q = torch.randn((8, 10, 32, 32))
 
-    out, logs = l_vqgan(ipt, ipt, latent_e, latent_q)       # train autoencoder
+    # loss for autoencoder
+    out, logs = l_vqgan(ipt, ipt, latent_e, latent_q)
     print("Output:", out)
     print("Logs:", logs)
 
-    out, logs = l_vqgan(ipt, ipt, latent_e, latent_q, train_autoencoder=False)       # train discriminator
+    # update discriminator
+    out, logs = l_vqgan.update_discriminator(ipt, ipt)
     print("Output:", out)
     print("Logs:", logs)
---
+++ train_vqgan_disc.py
@@ -0,0 +1,217 @@
+import os
+import pathlib
+import sys
+import time
+import yaml
+import argparse
+from tqdm import tqdm
+from datetime import datetime
+import torch
+
+from model.vqgan import VQGAN
+from model.losses import LossFn
+from utils.logger import Logger
+from utils.helpers import timer
+from utils.helpers import load_model_checkpoint, save_model_checkpoint
+from utils.helpers import log2tensorboard_vqvae
+from utils.visualization import get_original_reconstruction_image
+from dataloader import CIFAR10, PlantNet
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
+CHECKPOINT_DIR = os.path.join(pathlib.Path(__file__).parent.resolve(), 'checkpoints')
+LOG_DIR = os.path.join(pathlib.Path(__file__).parent.resolve(), 'logs')
+TIMESTAMP = datetime.now().strftime('%y-%m-%d_%H%M%S')
+
+
+parser = argparse.ArgumentParser(description="PyTorch First Stage Training")
+parser.add_argument('--name', '-n', default='',
+                    type=str, metavar='NAME', help='Model name and folder where logs are stored')
+parser.add_argument('--epochs', default=2,
+                    type=int, metavar='N', help='Number of epochs to run (default: 2)')
+parser.add_argument('--batch-size', default=64, metavar='N',
+                    type=int, help='Mini-batch size (default: 64)')
+parser.add_argument('--image-size', default=128, metavar='N',
+                    type=int, help='Size that images should be resized to before processing (default: 128)')
+parser.add_argument('--num-workers', default=0, metavar='N',
+                    type=int, help='Number of workers for the dataloader (default: 0)')
+parser.add_argument('--lr', default=0.0001,
+                    type=float, metavar='LR', help='Initial learning rate (default: 0.0001)')
+parser.add_argument('--config', default='configs/vqgan.yaml',
+                    metavar='PATH', help='Path to model config file (default: configs/vqgan.yaml)')
+parser.add_argument('--data-config', default='configs/data_se.yaml',
+                    metavar='PATH', help='Path to model config file (default: configs/data_se.yaml)')
+parser.add_argument('--debug', action='store_true',
+                    help='If true, trains on CIFAR10')
+parser.add_argument('--gpus', default=0, type=int,
+                    nargs='+', metavar='GPUS', help='If GPU(s) available, which GPU(s) to use for training.')
+parser.add_argument('--ckpt-save', default=True, action=argparse.BooleanOptionalAction,
+                    dest='save_checkpoint', help='Save checkpoints to folder')
+parser.add_argument('--load-ckpt', default=None, metavar='PATH',
+                    dest='load_checkpoint', help='Load model checkpoint and continue training')
+parser.add_argument('--log-save-interval', default=5, type=int, metavar='N',
+                    dest='save_interval', help="Interval in which logs are saved to disk (default: 5)")
+
+
+logger = Logger(LOG_DIR)
+
+
+def main():
+    args = parser.parse_args()
+    for name, val in vars(args).items():
+        print("{:<16}: {}".format(name, val))
+
+    # setup paths and logging
+    args.name = 'vqgan/' + args.name
+    running_log_dir = os.path.join(LOG_DIR, args.name, f'{TIMESTAMP}')
+    running_ckpt_dir = os.path.join(CHECKPOINT_DIR, args.name, f'{TIMESTAMP}')
+    print("{:<16}: {}".format('logdir', running_log_dir))
+    print("{:<16}: {}".format('ckpt_dir', running_ckpt_dir))
+
+    if args.save_checkpoint and not os.path.exists(running_ckpt_dir):
+        os.makedirs(running_ckpt_dir)
+
+    global logger
+    logger = Logger(running_log_dir, tensorboard=True)
+
+    # setup GPU
+    if len(args.gpus) == 1:
+        device = torch.device(f'cuda:{args.gpus[0]}' if torch.cuda.is_available() else 'cpu')
+    else:
+        raise ValueError('Currently multi-gpu training is not possible')
+
+    # load data
+    if args.debug:
+        data = CIFAR10(args.batch_size)
+    else:
+        data_cfg = yaml.load(open(args.data_config, 'r'), Loader=yaml.Loader)
+        data = PlantNet(**data_cfg, batch_size=args.batch_size,
+                        image_size=args.image_size, num_workers=args.num_workers)
+
+    # read config file for model
+    cfg = yaml.load(open(args.config, 'r'), Loader=yaml.Loader)
+
+    # create model and optimizer
+    model = VQGAN(**cfg['model'])
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), args.lr)
+
+    criterion = LossFn(**cfg['loss'])
+    # TODO
+    # set discriminator weight to 0 as we start updating it only at a later stage
+    # (see https://arxiv.org/abs/2012.09841).
+    # criterion.disc_weight = 0
+    criterion.to(device)
+
+    # resume training
+    if args.load_checkpoint:
+        model, start_epoch, global_train_step = load_model_checkpoint(model, args.load_checkpoint, device)
+        logger.global_train_step = global_train_step
+        args.epochs += start_epoch
+    else:
+        start_epoch = 0
+
+    # start run
+    logger.log_hparams({**cfg, **vars(args)})
+    t_start = time.time()
+    for epoch in range(start_epoch, args.epochs):
+        logger.init_epoch(epoch)
+        print(f"Epoch [{epoch + 1} / {args.epochs}]")
+
+        train(model, data.train, optimizer, criterion, device)
+
+        validate(model, data.val, criterion, device)
+
+        # logging
+        output = ' - '.join([f'{k}: {v.avg:.4f}' for k, v in logger.epoch.items()])
+        print(output)
+
+        # save logs and checkpoint
+        if (epoch + 1) % args.save_interval == 0 or (epoch + 1) == args.epochs:
+            logger.save()
+            if args.save_checkpoint:
+                save_model_checkpoint(model, running_ckpt_dir, logger)
+
+    elapsed_time = timer(t_start, time.time())
+    print(f"Total training time: {elapsed_time}")
+
+
+def train(model, train_loader, optimizer, criterion, device):
+    model.train()
+
+    # In https://arxiv.org/abs/2012.09841 they set the factor for the adversarial loss
+    # to zero for the first iterations (suggestion: at least one epoch). Longer warm-ups
+    # generally lead to better reconstructions.
+    warm_up_iters = 300
+
+    logs_keys = None
+    for x, _ in tqdm(train_loader, desc="Training"):
+        x = x.to(device)
+
+        x_hat, z_e, z_q = model(x)
+
+        # compute loss
+        # TODO: if iter < threshold, dont update D
+        loss, logs = criterion(x_hat, x, z_e, z_q)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # update the discriminator
+        _, disc_logs = criterion.update_discriminator(x_hat, x)
+        logs.update(disc_logs)
+        if logs_keys is None:
+            logs_keys = logs.keys()
+
+        logger.log_metrics(logs, phase='train', aggregate=True, n=x.shape[0])
+
+        if logger.global_train_step % 150 == 0:
+            log2tensorboard_vqvae(logger, 'Train', logs_keys)
+            ims = get_original_reconstruction_image(x, x_hat, n_ims=8)
+            logger.tensorboard.add_image('Train: Original vs. Reconstruction', ims,
+                                         global_step=logger.global_train_step, dataformats='HWC')
+
+        logger.global_train_step += 1
+
+    log2tensorboard_vqvae(logger, 'Train', logs_keys)
+
+
+@torch.no_grad()
+def validate(model, val_loader, criterion, device):
+    model.eval()
+
+    is_first = True
+    logs_keys = None
+    for x, _ in tqdm(val_loader, desc="Validation"):
+        x = x.to(device)
+
+        x_hat, z_e, z_q = model(x)
+
+        # compute loss
+        loss, logs = criterion(x_hat, x, z_e, z_q)
+
+        # logging
+        logs = {'val_' + k: v for k, v in logs.items()}
+        if logs_keys is None:
+            logs_keys = logs.keys()
+        logger.log_metrics(logs, phase='val', aggregate=True, n=x.shape[0])
+
+        if is_first:
+            is_first = False
+            ims = get_original_reconstruction_image(x, x_hat, n_ims=8)
+            logger.tensorboard.add_image('Val: Original vs. Reconstruction', ims,
+                                         global_step=logger.global_train_step, dataformats='HWC')
+
+    log2tensorboard_vqvae(logger, 'Val', logs_keys)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Exit training with keyboard interrupt!")
+        logger.save()
+        sys.exit(0)
